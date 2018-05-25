e0be9becd89426ee30e2859355ba8f228b503a9e
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-05-24 21:50:08 -0500

    yoga prisma

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
+  },
+  Mutation: {
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
+    },
+  },
+}
+
+
+const server = new GraphQLServer({
+  typeDefs:  './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
